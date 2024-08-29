6cb3fe65d5b067f254b540665d22cf933c54f9b6
Dylan2407 <123521382+Dylan2407@users.noreply.github.com>
2024-08-29 09:40:10 -0700

    Update Direccion.sql
    Se agrega paquete a direccion

--- SQL/Direccion.sql
+++ SQL/Direccion.sql
@@ -11,114 +11,146 @@ EXCEPTION
     DBMS_OUTPUT.PUT_LINE('LA TABLA ' || tabla || ' NO EXISTE');
 END;
 /
--- TABLA DE EMPLEADOS
+-- TABLA DE DIRECCION
 CREATE TABLE TBL_DIRECCION(
     ID_DIRECCION NUMBER NOT NULL PRIMARY KEY,
     DIRECCIONCOMPLETA VARCHAR2(50) NOT NULL,
     ID_DISTRITO NUMBER NOT NULL
 );
 
--- CRUD PARA LA TABLA DE DIRECCION
--- PROCEDIMIENTO PARA REGISTRAR DIRECCIONES
-CREATE OR REPLACE PROCEDURE SP_INSERTAR_DIRECCION(
-    P_ID_DIRECCION TBL_DIRECCION.ID_DIRECCION%TYPE,
-    P_DIRECCIONCOMPLETA TBL_DIRECCION.DIRECCIONCOMPLETA%TYPE,
-    P_ID_DISTRITO TBL_DIRECCION.ID_DISTRITO%TYPE
-)AS
-    V_COUNT NUMBER;
-BEGIN
-    SELECT COUNT(*)
-    INTO V_COUNT
-    FROM TBL_DIRECCION
-    WHERE ID_DIRECCION = P_ID_DIRECCION;
-    
-    IF V_COUNT > 0 THEN
-        DBMS_OUTPUT.PUT_LINE('YA EXISTE UN REGISTRO CON EL ID ' || P_ID_DIRECCION);
-    ELSE
-        INSERT INTO TBL_DIRECCION(ID_DIRECCION,DIRECCIONCOMPLETA,ID_DISTRITO)
-        VALUES (P_ID_DIRECCION,P_DIRECCIONCOMPLETA,P_ID_DISTRITO);
-        COMMIT;
-        DBMS_OUTPUT.PUT_LINE('REGISTRO REALIZADO');
-    END IF;
-END;
+CREATE OR REPLACE PACKAGE pkg_direccion_utilidades AS
+    -- Declaración de los procedimientos
+    PROCEDURE sp_insertar_direccion(
+        p_id_direccion TBL_DIRECCION.ID_DIRECCION%TYPE,
+        p_direccioncompleta TBL_DIRECCION.DIRECCIONCOMPLETA%TYPE,
+        p_id_distrito TBL_DIRECCION.ID_DISTRITO%TYPE
+    );
+
+    PROCEDURE sp_leer_direccion(
+        p_id_direccion NUMBER
+    );
+
+    PROCEDURE sp_actualizar_direccion(
+        p_id_direccion TBL_DIRECCION.ID_DIRECCION%TYPE,
+        p_direccioncompleta TBL_DIRECCION.DIRECCIONCOMPLETA%TYPE,
+        p_id_distrito TBL_DIRECCION.ID_DISTRITO%TYPE
+    );
+
+    PROCEDURE sp_eliminar_direccion(
+        p_id_direccion TBL_DIRECCION.ID_DIRECCION%TYPE
+    );
+END pkg_direccion_utilidades;
 /
 
--- PROCEDIMIENTO PARA LEER DATOS DE UNA DIRECCION
-CREATE OR REPLACE PROCEDURE SP_LEER_DIRECCION(
-    P_ID_DIRECCION NUMBER
-)IS
-    V_ID_DIRECCION TBL_DIRECCION.ID_DIRECCION%TYPE;
-    V_DIRECCIONCOMPLETA TBL_DIRECCION.DIRECCIONCOMPLETA%TYPE;
-    V_ID_DISTRITO TBL_DIRECCION.ID_DISTRITO%TYPE;
-BEGIN    
-    SELECT ID_DIRECCION, DIRECCIONCOMPLETA, ID_DISTRITO INTO V_ID_DIRECCION, V_DIRECCIONCOMPLETA, V_ID_DISTRITO
-    FROM TBL_DIRECCION
-    WHERE ID_DIRECCION = P_ID_DIRECCION;
-    
-    DBMS_OUTPUT.PUT_LINE('ID DE LA DIRECCION: ' || V_ID_DIRECCION || ', DIRECCION COMPLETA: ' || V_DIRECCIONCOMPLETA || ', ID_DISTRITO: ' || V_ID_DISTRITO);
-    
-EXCEPTION
-    WHEN NO_DATA_FOUND THEN
-    DBMS_OUTPUT.PUT_LINE('LA DIRECCION CON EL ID ' || P_ID_DIRECCION || ' NO FUE ENCONTRADA.');
-END;
+CREATE OR REPLACE PACKAGE BODY pkg_direccion_utilidades AS
+
+    -- Implementación del procedimiento para insertar direcciones
+    PROCEDURE sp_insertar_direccion(
+        p_id_direccion TBL_DIRECCION.ID_DIRECCION%TYPE,
+        p_direccioncompleta TBL_DIRECCION.DIRECCIONCOMPLETA%TYPE,
+        p_id_distrito TBL_DIRECCION.ID_DISTRITO%TYPE
+    ) AS
+        v_count NUMBER;
+    BEGIN
+        SELECT COUNT(*)
+        INTO v_count
+        FROM TBL_DIRECCION
+        WHERE ID_DIRECCION = p_id_direccion;
+        
+        IF v_count > 0 THEN
+            DBMS_OUTPUT.PUT_LINE('YA EXISTE UN REGISTRO CON EL ID ' || p_id_direccion);
+        ELSE
+            INSERT INTO TBL_DIRECCION(ID_DIRECCION, DIRECCIONCOMPLETA, ID_DISTRITO)
+            VALUES (p_id_direccion, p_direccioncompleta, p_id_distrito);
+            COMMIT;
+            DBMS_OUTPUT.PUT_LINE('REGISTRO REALIZADO');
+        END IF;
+    END sp_insertar_direccion;
+
+    -- Implementación del procedimiento para leer direcciones
+    PROCEDURE sp_leer_direccion(
+        p_id_direccion NUMBER
+    ) IS
+        v_id_direccion TBL_DIRECCION.ID_DIRECCION%TYPE;
+        v_direccioncompleta TBL_DIRECCION.DIRECCIONCOMPLETA%TYPE;
+        v_id_distrito TBL_DIRECCION.ID_DISTRITO%TYPE;
+    BEGIN    
+        SELECT ID_DIRECCION, DIRECCIONCOMPLETA, ID_DISTRITO INTO v_id_direccion, v_direccioncompleta, v_id_distrito
+        FROM TBL_DIRECCION
+        WHERE ID_DIRECCION = p_id_direccion;
+        
+        DBMS_OUTPUT.PUT_LINE('ID DE LA DIRECCION: ' || v_id_direccion || ', DIRECCION COMPLETA: ' || v_direccioncompleta || ', ID_DISTRITO: ' || v_id_distrito);
+        
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            DBMS_OUTPUT.PUT_LINE('LA DIRECCION CON EL ID ' || p_id_direccion || ' NO FUE ENCONTRADA.');
+    END sp_leer_direccion;
+
+    -- Implementación del procedimiento para actualizar direcciones
+    PROCEDURE sp_actualizar_direccion(
+        p_id_direccion TBL_DIRECCION.ID_DIRECCION%TYPE,
+        p_direccioncompleta TBL_DIRECCION.DIRECCIONCOMPLETA%TYPE,
+        p_id_distrito TBL_DIRECCION.ID_DISTRITO%TYPE
+    ) AS
+        v_count NUMBER;
+    BEGIN
+        SELECT COUNT(*)
+        INTO v_count
+        FROM TBL_DIRECCION
+        WHERE ID_DIRECCION = p_id_direccion;
+
+        IF v_count > 0 THEN
+            UPDATE TBL_DIRECCION
+            SET DIRECCIONCOMPLETA = p_direccioncompleta, ID_DISTRITO = p_id_distrito
+            WHERE ID_DIRECCION = p_id_direccion;
+            COMMIT;
+
+            DBMS_OUTPUT.PUT_LINE('LA DIRECCION CON EL ID ' || p_id_direccion || ' HA SIDO ACTUALIZADA.');
+        ELSE
+            DBMS_OUTPUT.PUT_LINE('LA DIRECCION CON EL ID ' || p_id_direccion || ' NO FUE ENCONTRADA.');
+        END IF;
+    END sp_actualizar_direccion;
+
+    -- Implementación del procedimiento para eliminar direcciones
+    PROCEDURE sp_eliminar_direccion(
+        p_id_direccion TBL_DIRECCION.ID_DIRECCION%TYPE
+    ) AS
+        v_count NUMBER;
+    BEGIN
+        SELECT COUNT(*)
+        INTO v_count
+        FROM TBL_DIRECCION
+        WHERE ID_DIRECCION = p_id_direccion;
+
+        IF v_count > 0 THEN
+            DELETE FROM TBL_DIRECCION
+            WHERE ID_DIRECCION = p_id_direccion;
+            COMMIT;
+
+            DBMS_OUTPUT.PUT_LINE('LA DIRECCION CON EL ID ' || p_id_direccion || ' FUE ELIMINADA CORRECTAMENTE.');
+        ELSE
+            DBMS_OUTPUT.PUT_LINE('LA DIRECCION CON EL ID ' || p_id_direccion || ' NO FUE ENCONTRADA.');
+        END IF;
+    END sp_eliminar_direccion;
+
+END pkg_direccion_utilidades;
 /
 
--- PROCEDIMIENTO PARA ACTUALIZAR UNA DIRECCION 
-CREATE OR REPLACE PROCEDURE SP_ACTUALIZAR_DIRECCION(
-    P_ID_DIRECCION TBL_DIRECCION.ID_DIRECCION%TYPE,
-    P_DIRECCIONCOMPLETA TBL_DIRECCION.DIRECCIONCOMPLETA%TYPE,
-    P_ID_DISTRITO TBL_DIRECCION.ID_DISTRITO%TYPE
-)AS
-    V_COUNT NUMBER;
-BEGIN
-    SELECT COUNT(*)
-    INTO V_COUNT
-    FROM TBL_DIRECCION
-    WHERE ID_DIRECCION = P_ID_DIRECCION;
+-- Bloque CRUD con SPs para TBL_DIRECCION usando el paquete pkg_direccion_utilidades
 
-    IF V_COUNT > 0 THEN
+-- Insertar una nueva dirección
+EXEC pkg_direccion_utilidades.sp_insertar_direccion(1, 'Avenida 1 Detras Del Palo De Mangos', 1);
+EXEC pkg_direccion_utilidades.sp_leer_direccion(1);
+EXEC pkg_direccion_utilidades.sp_actualizar_direccion(1, 'Avenida 2 frente a la planta de chayotes', 1);
 
-        UPDATE TBL_DIRECCION
-        SET ID_DIRECCION = P_ID_DIRECCION, DIRECCIONCOMPLETA = P_DIRECCIONCOMPLETA, ID_DISTRITO = P_ID_DISTRITO
-        WHERE ID_DIRECCION = P_ID_DIRECCION;
-        COMMIT;
+--EXEC pkg_direccion_utilidades.sp_eliminar_direccion(1);
 
-        DBMS_OUTPUT.PUT_LINE('LA DIRECCION CON EL ID ' || P_ID_DIRECCION || ' HA SIDO ACTUALIZADO.');
+-- Insertar direcciones adicionales
+EXEC pkg_direccion_utilidades.sp_insertar_direccion(1, 'Avenida 1 Detras Del Palo De Mangos', 1);
+EXEC pkg_direccion_utilidades.sp_insertar_direccion(2, 'Calle Hernandez frente al taller Juanito', 1);
 
-    ELSE 
 
-        DBMS_OUTPUT.PUT_LINE('LA DIRECCION CON EL ID ' || P_ID_DIRECCION || ' NO FUE ENCONTRADO.');
 
-    END IF;
-END;
-/
-
--- PRECEDIMIENTO PARA ELIMINAR UN EMPLEADO
-CREATE OR REPLACE PROCEDURE SP_ELIMINAR_DIRECCION(
-    P_ID_DIRECCION TBL_DIRECCION.ID_DIRECCION%TYPE
-)AS
-    V_COUNT NUMBER;
-BEGIN
-    SELECT COUNT(*)
-    INTO V_COUNT
-    FROM TBL_DIRECCION
-    WHERE ID_DIRECCION = P_ID_DIRECCION;
-
-    IF V_COUNT > 0 THEN
-
-        DELETE FROM TBL_DIRECCION
-        WHERE ID_DIRECCION = P_ID_DIRECCION;
-        COMMIT;
-
-        DBMS_OUTPUT.PUT_LINE('LA DIRECCION CON EL ID ' || P_ID_DIRECCION || ' FUE ELIMINADA CORRECTAMENTE.');
-
-    ELSE
-
-        DBMS_OUTPUT.PUT_LINE('LA DIRECCION CON EL ID ' || P_ID_DIRECCION || ' NO FUE ENCONTRADA.');
-
-    END IF;
-END;
-/
 
 --VISTA PARA LISTAR TODAS LAS DIRECCIONES
 CREATE OR REPLACE VIEW VISTA_LISTAR_DIRECCIONES AS 
@@ -127,15 +159,6 @@ SELECT ID_DIRECCION, Upper(DIRECCIONCOMPLETA) "DIRECCION COMPLETA", ID_DISTRITO 
 --VISTA PARA LISTAR CANTIDAD DE DIRECCIONES
 CREATE OR REPLACE VIEW VISTA_CANTIDAD_DE_DIRECCIONES AS 
 SELECT COUNT(ID_DIRECCION) "CANTIDAD DE DIRECCIONES" FROM TBL_DIRECCION WITH READ ONLY;
-
--- Bloque CRUD con SPs para TBL_DIRECCION
-EXEC SP_INSERTAR_DIRECCION(1, 'Avenida 1 Detras Del Palo De Mangos', 1);
-EXEC sp_leer_direccion(1);
-EXEC sp_actualizar_direccion(1, 'Avenida 2 frente a la planta de chayotes', 1);
---EXEC sp_eliminar_direccion(1);
-
-EXEC SP_INSERTAR_DIRECCION(1, 'Avenida 1 Detras Del Palo De Mangos', 1);
-EXEC SP_INSERTAR_DIRECCION(2, 'Calle Hernandez frente al taller Juanito', 1);
 
 -- Ejecucion de la vista Direccion
 SELECT "DIRECCION COMPLETA", ID_DISTRITO FROM vista_listar_direcciones;
